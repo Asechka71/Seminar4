6a8a0f679fafe965a4aadf6c099175ef4d2cfcae
Anastasiia <kariakina.nastia@example.com>
2022-08-19 18:17:30 +0300

    add Task 29

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+(на примерах демонстрация выводов 5 и 3 размерных массивов, вам же нужно сделать 8)
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите массив через запятую ");
+int[] collectionPrint = Array.ConvertAll<string,int>
+(Console.ReadLine().Split(','), elem => { return System.Convert.ToInt32 (elem);});
+
+void PrintArray(int[] collectionPrint)
+{
+    int count = collectionPrint.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count-1)
+    {
+      Console.Write(collectionPrint[position]+ ", ");
+      position++;
+    }
+    Console.Write(collectionPrint[position] +"]");
+}
+PrintArray(collectionPrint);
